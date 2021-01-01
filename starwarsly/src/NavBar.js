475e4daf21fd0ae3e232b199d11b14fcfb191e98
Christopher Klint <christopher.klint@gmail.com>
2021-01-01 16:58:17 +0100

    quick fixes

--- starwarsly/src/NavBar.js
+++ starwarsly/src/NavBar.js
@@ -1,6 +1,7 @@
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import "./NavBar.css"
+import "./NavBar.css";
 
 /**
  * Navbar for app, providing links to:
@@ -13,24 +14,37 @@ import "./NavBar.css"
 function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <NavLink className="navbar-brand" exact to="/">StarWars.ly</NavLink>
-      <button className="navbar-toggler" type="button" data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false"
-              aria-label="Toggle navigation">
+      <NavLink className="navbar-brand" exact to="/">
+        StarWars.ly
+      </NavLink>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon" />
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <NavLink className="nav-link" to="/films">Films</NavLink>
+            <NavLink className="nav-link" to="/films">
+              Films
+            </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/planets">Planets</NavLink>
+            <NavLink className="nav-link" to="/planets">
+              Planets
+            </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/people">People</NavLink>
+            <NavLink className="nav-link" to="/people">
+              People
+            </NavLink>
           </li>
         </ul>
       </div>
@@ -38,5 +52,4 @@ function NavBar() {
   );
 }
 
-
-export default NavBar
+export default NavBar;
